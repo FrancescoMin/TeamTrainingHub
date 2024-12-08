cc9252909688186c9fa9deada79b9fcc7fe5c08b
Simone2oo2 <simone.manisi002@gmail.com>
2024-12-08 19:45:02 +0100

    Creazione Classe Cambioscena, paginaPrincipale con controller senza funzionalità

--- src/main/java/viste/first/utils/CambioScena.java
+++ src/main/java/viste/first/utils/CambioScena.java
@@ -5,15 +5,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class CambioScena {
     public void cambioScena(Stage stage, String string) {
         try {
 
-            Parent root = FXMLLoader.load(getClass().getResource(string));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(string)));
             stage.setScene(new Scene(root));
             stage.show();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Errore nel cambio scena");
         }
     }
 }
