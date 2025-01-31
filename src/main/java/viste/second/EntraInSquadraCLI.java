25cb1b04472dabb4127e745dc081a6249951fc25
Simone2oo2 <simone.manisi002@gmail.com>
2025-01-31 14:24:17 +0100

    EntraInSquadraCLI implementazione

--- src/main/java/viste/second/EntraInSquadraCLI.java
+++ src/main/java/viste/second/EntraInSquadraCLI.java
@@ -1,5 +1,7 @@
 package viste.second;
 
+import ctrlApplicativo.EntraInSquadraCtrlApplicativo;
+
 public class EntraInSquadraCLI extends GenericaCLI {
     public EntraInSquadraCLI() {
         this.pagina= "Entra In Squadra";
@@ -7,6 +9,44 @@ public class EntraInSquadraCLI extends GenericaCLI {
 
     @Override
     public void start() {
+
+        String nomeSquadra;
         stampaPagina();
+
+        //facciamo un ciclo dove insegnamo il nome di una squadra valida oppure exit per tornare indietro
+        do {
+            System.out.println("Per entrare in una squadra, inserisci il nome della squadra. Se si vuole tornare sulla pagina precedente, inserire exit.");
+
+            //prendiamo l'ingresso dell'utente
+            nomeSquadra = scanner.nextLine();
+
+            //controlliamo se volgiamo uscire
+            if (nomeSquadra.equals("exit")) {
+                prossimaPagina = HomepageGiocatoreCLI.class.getName();
+            }
+            //se non vogliamo uscire lavoriamo con il nome della squadra
+            else {
+                try {
+                    EntraInSquadraCtrlApplicativo entraInSquadraCtrlApplicativo = new EntraInSquadraCtrlApplicativo();
+
+                    //entriamo se la squadra esiste
+                    if (entraInSquadraCtrlApplicativo.verificaEsistenzaSquadra(nomeSquadra)) {
+                        entraInSquadraCtrlApplicativo.inviaRichiestaAllaSquadra(nomeSquadra);
+                        System.out.println("Richiesta inviata con successo.");
+                        prossimaPagina = HomepageGiocatoreCLI.class.getName();
+                    }
+                    //se la squadra non esiste entriamo qui
+                    else {
+                        System.out.println("La squadra non esiste.");
+                    }
+                }
+                //gestiamo le eccezioni qui
+                catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        while(prossimaPagina.isEmpty());
+        spostamento(prossimaPagina);
     }
 }
