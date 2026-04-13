2bfa74b9d60bd344a5a4b8c1e9952cab3dd7344c
FrancescoMin <francescominotti2001@gmail.com>
2026-04-13 15:18:56 +0200

    fix import

--- src/main/java/engineering/dao/AllenamentoDAOInMemory.java
+++ src/main/java/engineering/dao/AllenamentoDAOInMemory.java
@@ -6,7 +6,6 @@ import modelli.Utente;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class AllenamentoDAOInMemory implements AllenamentoDAO {
 
